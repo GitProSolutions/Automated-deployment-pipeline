2dd290bb03ee127bcb89c9cd13024f13759bbfae
GitPro <124433843+GitProSolutions@users.noreply.github.com>
2023-02-04 16:20:08 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,40 +1,49 @@
 #!/bin/bash
 
 # Update the server
+
 sudo apt-get update -y
 
 # Install Jenkins
+
 sudo apt-get install -y default-jdk
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install -y jenkins
 
-# Start Jenkins
+# Step 1: Start Jenkins
+
 sudo systemctl start jenkins
 
-# Enable auto-start for Jenkins
+# Step 2: Enable auto-start for Jenkins
+
 sudo systemctl enable jenkins
 
-# Install Git
+# Step 3: Install Git
+
 sudo apt-get install -y git
 
-# Clone the web application repository
+# Step 4: Clone the web application repository
+
 git clone https://github.com/[YOUR_USERNAME]/[YOUR_REPO].git
 
-# Install Apache
+# Step 5: Install Apache
+
 sudo apt-get install -y apache2
 
-# Copy the application files to the Apache root directory
+# Step 6: Copy the application files to the Apache root directory
+
 sudo cp -r [YOUR_REPO]/* /var/www/html/
 
-# Restart Apache
+# Step 7: Restart Apache
+
 sudo systemctl restart apache2
 
-# Create a Jenkins job to automate the deployment process
-# Open your web browser and navigate to http://<your_server_ip>:8080
-# Follow the steps to set up Jenkins and create a new job
-# In the "Build Triggers" section, select "Poll SCM" and specify a schedule (e.g. H/15 * * * *)
-# In the "Build" section, select "Execute shell" and enter the following command:
-# sudo cp -r [YOUR_REPO]/* /var/www/html/ && sudo systemctl restart apache2
-# Save the job and build it to test the deployment pipeline
+# Step 8:  Create a Jenkins job to automate the deployment process
+# Step 9:  Open your web browser and navigate to http://<your_server_ip>:8080
+# Step 10: Follow the steps to set up Jenkins and create a new job
+# Step 11: In the "Build Triggers" section, select "Poll SCM" and specify a schedule (e.g. H/15 * * * *)
+# Step 12: In the "Build" section, select "Execute shell" and enter the following command:
+# Step 13: sudo cp -r [YOUR_REPO]/* /var/www/html/ && sudo systemctl restart apache2
+# Step 14: Save the job and build it to test the deployment pipeline
